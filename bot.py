9da74e46b85ddde2b2c7c66b6c838be9e94b1aea
timih18 <116382351+timih18@users.noreply.github.com>
2023-03-05 16:34:32 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,154 +1,170 @@
 import telebot
+from telebot import types
 import schedule
 from threading import Thread
-from dict import data
-from telebot import types
 from TOKEN import token
 
-
-########################################################################################################################
 bot = telebot.TeleBot(token)
-photo = 'https://media.discordapp.net/attachments/1020346164505219092/1063730044075261983/IMG_20230114_110257_603.jpg'
-chat_id = 0
+data = {'cnt_start': 0, 'cnt_plants': 0}
+plant = ''
 
 
-########################################################################################################################
 @bot.message_handler(commands=['start'])
 def start(message):
-    global chat_id
-    chat_id = message.chat.id
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    plant_1 = types.InlineKeyboardButton('Растение 1', callback_data='plant_1')
-    plant_2 = types.InlineKeyboardButton('Растение 2', callback_data='plant_2')
-    plant_3 = types.InlineKeyboardButton('Растение 3', callback_data='plant_3')
-    plant_4 = types.InlineKeyboardButton('Растение 4', callback_data='plant_4')
-    plant_5 = types.InlineKeyboardButton('Растение 5', callback_data='plant_5')
-    plant_6 = types.InlineKeyboardButton('Растение 6', callback_data='plant_6')
-    markup.add(plant_1, plant_2, plant_3, plant_4, plant_5, plant_6)
-
-    text1 = 'Привет! Это бот по уходу за цветами школы 1561.'
-    text2 = 'Я буду присылать напоминания о необходимости полить цветы. Также ты можешь написать '
-    text3 = '/(номер на горшке), чтобы узнать информацию о растении.'
-    text = text1+'\n'+text2+'\n'+text3
-    bot.send_photo(chat_id, photo, caption=text, reply_markup=markup)
+    global data
+    if data['cnt_start'] == 0:
+        # todo: Сделать двойной словарь для разных чатов
+        data['admin_id'] = message.from_user.id
+        data['chat_id'] = message.chat.id
+        data['cnt_start'] = 1
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    admin_button = types.InlineKeyboardButton('Настройка бота', callback_data='admin')
+    markup.add(admin_button)
+    text_start = 'Привет! Это бот по уходу за цветами школы 1561. Для настройки нажми на кнопку. ' \
+                 'Список команд:' + '\n' + '~ /admin для настройки бота' + '\n' + '~ /list для вывода списка растений'
+    bot.send_message(message.chat.id, text_start, reply_markup=markup)
 
 
-########################################################################################################################
-@bot.message_handler()
-def number(message):
-    global chat_id
-    chat_id = message.chat.id
-    if message.text == '/1' or message.text == '/1@sch1561_plants_bot':
-        plant1()
-    elif message.text == '/2' or message.text == '/2@sch1561_plants_bot':
-        plant2()
-    elif message.text == '/3' or message.text == '/3@sch1561_plants_bot':
-        plant3()
-    elif message.text == '/4' or message.text == '/4@sch1561_plants_bot':
-        plant4()
-    elif message.text == '/5' or message.text == '/5@sch1561_plants_bot':
-        plant5()
-    elif message.text == '/6' or message.text == '/6@sch1561_plants_bot':
-        plant6()
-
-
-########################################################################################################################
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
+    global data
     if call.message:
-        if call.data == 'plant_1':
-            plant1()
-        elif call.data == 'plant_2':
-            plant2()
-        elif call.data == 'plant_3':
-            plant3()
-        elif call.data == 'plant_4':
-            plant4()
-        elif call.data == 'plant_5':
-            plant5()
-        elif call.data == 'plant_6':
-            plant6()
-        elif call.data == 'care_person':
-            markup = types.InlineKeyboardMarkup(row_width=1)
-            done = types.InlineKeyboardButton('Все полито!', callback_data='done')
-            markup.add(done)
-            text = f'<b>{call.from_user.first_name}</b> польет растения.'
-            bot.send_message(chat_id, text, parse_mode='html', reply_markup=markup)
-        elif call.data == 'done':
-            bot.send_message(chat_id, 'Все растения политы!')
+        if call.from_user.id == data['admin_id']:
+            if call.data == 'admin':
+                admin(call.message)
+            elif call.data == 'add_plant':
+                add_plant(call.message)
+            elif call.data == 'change_time':
+                msg = bot.send_message(call.message.chat.id, 'Введи через "/" номер растения, для которого хочешь '
+                                                             'изменить время полива.')
+                bot.register_next_step_handler(msg, after_change_time)
+            elif call.data == 'delete':
+                msg = bot.send_message(call.message.chat.id, 'Введи через "/" номер растения, которое хочешь удалить.')
+                bot.register_next_step_handler(msg, after_delete)
+        else:
+            bot.send_message(call.message.chat.id, 'Ты не администратор этого бота!')
 
 
-########################################################################################################################
-def reminder1():
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    plant_1 = types.InlineKeyboardButton('Растение 1', callback_data='plant_1')
-    plant_2 = types.InlineKeyboardButton('Растение 2', callback_data='plant_2')
-    plant_3 = types.InlineKeyboardButton('Растение 3', callback_data='plant_3')
-    care_person = types.InlineKeyboardButton('Я полью', callback_data='care_person')
-    markup.add(plant_1, plant_2, plant_3, care_person)
-
-    bot.send_photo(chat_id, photo, caption='Нужно полить растения 1, 2 и 3!', reply_markup=markup)
-
-
-def reminder2():
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    plant_4 = types.InlineKeyboardButton('Растение 4', callback_data='plant_4')
-    plant_5 = types.InlineKeyboardButton('Растение 5', callback_data='plant_5')
-    plant_6 = types.InlineKeyboardButton('Растение 6', callback_data='plant_6')
-    care_person = types.InlineKeyboardButton('Я полью', callback_data='care_person')
-    markup.add(plant_4, plant_5, plant_6, care_person)
-
-    bot.send_photo(chat_id, photo, caption='Нужно полить растения 4, 5 и 6!', reply_markup=markup)
-
-
-########################################################################################################################
-def plant1():
-    photo1 = data['1']['pict']
-    bot.send_photo(chat_id, photo1, caption=data['1']['name'] + '\n' + data['1']['desc'])
-
-
-def plant2():
-    photo2 = data['2']['pict']
-    bot.send_photo(chat_id, photo2, caption=data['2']['name'] + '\n' + data['2']['desc'])
+@bot.message_handler()
+def text(message):
+    global data
+    if message.text == '/admin' or message.text == '/admin@sch1561_plants_bot':
+        if message.from_user.id == data['admin_id']:
+            admin(message)
+        else:
+            bot.send_message(message.chat.id, 'Ты не администратор этого бота!')
+    if message.text[1:] in data:
+        msg = f'<b>{message.text[1:]}</b>' + '<b>.</b> ' + f'<b>{data[message.text[1:]]["name"]}</b>' + '\n' + \
+              'Поливать каждые ' + str(data[message.text[1:]]['time_of_feeding']) + ' дня/дней' + '\n' + 'Описание: ' +\
+              data[message.text[1:]]['description'] + '\n' + 'Кабинеты: ' + data[message.text[1:]]['rooms']
+        bot.send_message(message.chat.id, msg, parse_mode='html')
+    if len(message.text) > 20:
+        command = message.text[:-19]
+        if command[1:] in data:
+            msg = f'<b>{command[1:]}</b>' + '<b>.</b> ' + f'<b>{data[command[1:]]["name"]}</b>' + '\n' + \
+                  'Поливать каждые ' + str(data[command[1:]]['time_of_feeding']) + ' дня/дней' + '\n' + 'Описание: ' + \
+                  data[command[1:]]['description'] + '\n' + 'Кабинеты: ' + data[command[1:]]['rooms']
+            bot.send_message(message.chat.id, msg, parse_mode='html')
+    if message.text == '/list' or message.text == '/list@sch1561_plants_bot':
+        if data['cnt_plants'] > 0:
+            text_msg = ''
+            for i in range(1, data['cnt_plants']+1):
+                text_msg = text_msg + str(i) + '. ' + f'<b>{data[str(i)]["name"]}</b>' + '\n'
+            bot.send_message(message.chat.id, text_msg, parse_mode='html')
+        else:
+            bot.send_message(message.chat.id, 'Ты не добавил не одного растения!')
 
 
-def plant3():
-    photo3 = data['3']['pict']
-    bot.send_photo(chat_id, photo3, caption=data['3']['name'] + '\n' + data['3']['desc'])
+def admin(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    add_plant_button = types.InlineKeyboardButton('Добавить растение', callback_data='add_plant')
+    change_time_button = types.InlineKeyboardButton('Изменить время полива', callback_data='change_time')
+    delete_button = types.InlineKeyboardButton('Удалить растение', callback_data='delete')
+    markup.add(add_plant_button, change_time_button, delete_button)
+    bot.send_message(message.chat.id, 'Что ты хочешь сделать?', reply_markup=markup)
 
 
-def plant4():
-    photo4 = data['4']['pict']
-    bot.send_photo(chat_id, photo4, caption=data['4']['name'] + '\n' + data['4']['desc'])
+def add_plant(message):
+    msg = bot.send_message(message.chat.id, 'Введи название растения через "/".')
+    bot.register_next_step_handler(msg, after_add_plant)
 
 
-def plant5():
-    photo5 = data['5']['pict']
-    bot.send_photo(chat_id, photo5, caption=data['5']['name'] + '\n' + data['5']['desc'])
+def after_add_plant(message):
+    global data
+    data_plant = {'name': message.text[1:]}
+    msg = bot.send_message(message.chat.id, 'Теперь введи краткое описание через "/".')
+    bot.register_next_step_handler(msg, after_add_plant_2)
+    data['cnt_plants'] += 1
+    data[str(data['cnt_plants'])] = data_plant
 
 
-def plant6():
-    photo6 = data['6']['pict']
-    bot.send_photo(chat_id, photo6, caption=data['6']['name'] + '\n' + data['6']['desc'])
+def after_add_plant_2(message):
+    global data
+    data[str(data['cnt_plants'])]['description'] = message.text[1:]
+    msg = bot.send_message(message.chat.id, 'Раз в сколько дней присылать уведомления о поливе? Введи число через "/".')
+    bot.register_next_step_handler(msg, after_add_plant_3)
 
 
-########################################################################################################################
+def after_add_plant_3(message):
+    global data
+    data[str(data['cnt_plants'])]['time_of_feeding'] = int(message.text[1:])
+    data[str(data['cnt_plants'])]['time_since_feed'] = 0
+    msg = bot.send_message(message.chat.id, 'Введи номера кабинетов, в которых будет стоять растение через "/".')
+    bot.register_next_step_handler(msg, after_add_plant_4)
+
+
+def after_add_plant_4(message):
+    global data
+    data[str(data['cnt_plants'])]['rooms'] = message.text[1:]
+    msg = f'Твое растение сохранено под номером {data["cnt_plants"]}. Ты можешь написать /{data["cnt_plants"]},' \
+          f' чтобы увидеть подробную информацию о растении.'
+    bot.send_message(message.chat.id, msg)
+
+
+def after_change_time(message):
+    global plant
+    plant = message.text[1:]
+    msg = bot.send_message(message.chat.id, 'Теперь введи новое время полива через "/".')
+    bot.register_next_step_handler(msg, after_change_time_2)
+
+
+def after_change_time_2(message):
+    global data, plant
+    data[plant]['time_of_feeding'] = int(message.text[1:])
+    data[plant]['time_since_feed'] = 0
+    bot.send_message(message.chat.id, 'Время полива изменено.')
+
+
+def after_delete(message):
+    global data
+    elem = message.text[1:]
+    for i in range(int(elem), data['cnt_plants']):
+        data[str(i)] = data[str(i+1)]
+    data.pop(str(data['cnt_plants']))
+    data['cnt_plants'] -= 1
+    bot.send_message(message.chat.id, 'Растение удалено. Номера растений, стоящих после него изменены. Напиши /list, '
+                                      'чтобы увидеть новые номера растений.')
+
+
+def reminder():
+    for i in range(1, data['cnt_plants'] + 1):
+        if 'time_since_feed' in data[str(i)]:
+            if data[str(i)]['time_of_feeding'] == data[str(i)]['time_since_feed']:
+                msg = f'Надо полить <b>{data[str(i)]["name"]}</b> (номер {i}).'
+                bot.send_message(data['chat_id'], msg, parse_mode='html')
+                data[str(i)]['time_since_feed'] = 0
+            else:
+                data[str(i)]['time_since_feed'] += 1
+
+
 def main():
-    schedule.every().monday.at('10:15').do(reminder1)
-    schedule.every().tuesday.at('10:15').do(reminder2)
-    schedule.every().thursday.at('10:15').do(reminder1)
-    schedule.every().friday.at('10:15').do(reminder2)
-########################################################################################################################
-    # Для примера
-    schedule.every(10).minutes.do(reminder1)
-    schedule.every(10).minutes.do(reminder2)
-########################################################################################################################
+    global data
+    schedule.every(1).seconds.do(reminder)
 
     while True:
         schedule.run_pending()
 
 
-########################################################################################################################
 thread = Thread(target=main)
 thread.start()
 bot.polling(none_stop=True)
